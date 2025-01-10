157305b9a959633958f8d7dd8dd589b4cc4cc2b5
Tongzkungz <tongs2004@gmail.com>
2025-01-10 22:35:25 +0700

    responsive home page

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -19,64 +19,65 @@ const HeroHome = () => {
 
 
   return (
-    <div className="font-mono">     
-        <p className="flex justify-center font-bold pt-8 text-4xl">
-          Welcome to my website
-        </p>
+    <div className="font-mono">
+      <p className="text-center font-bold pt-8 text-4xl">
+        Welcome to my website
+      </p>
 
-        <div className="flex justify-center pt-8 text-4xl">
-          <TypeAnimation
-            className="inline-block text-3xl"
-            sequence={[
-              // Same substring at the start will only be typed out once, initially
-              "I am interested in Web application",
-              1000, // wait 1s before replacing "Mice" with "Hamsters"
-              "I am interested in Frontend development",
-              1000,
-              "I am interested in Software Engineering",
-              1000,
-              "I am interested in UX/UI design",
-              1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
+      <div className="text-center pt-8 text-4xl">
+        <TypeAnimation
+          className="inline-block text-3xl"
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            "I am interested in Web application",
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            "I am interested in Frontend development",
+            1000,
+            "I am interested in Software Engineering",
+            1000,
+            "I am interested in UX/UI design",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          repeat={Infinity}
+        />
+      </div>
+      <div className="md:grid grid-col-2 pt-10">
+        <div className="flex justify-center md:block md:col-start-1 md:ml-20">
+          <img
+            className="rounded-2xl w-[12rem] h-[16rem] md:w-[16rem] md:h-[20rem]"
+            src={mypic}
+            alt="my profile picture"
           />
         </div>
-        <div className="grid grid-col-2 pt-10">
-          <div className="col-start-1 ml-20">
-            <img
-              className="rounded-2xl w-[16rem] h-[20rem]"
-              src={mypic}
-              alt="my profile picture"
-            />
-          </div>
-          <div className="col-start-2 mr-20">
-            <p className="text-xl font-semibold pb-3">About me</p>
-            My name is Supanut Wongtanom. I am studying at the faculty <br />
-            of computer engineering in the 3rd year of international <br />
-            program at KMUTT at the moment. I am interested in front-end <br />
-            development especially React JS, NextJS and CSS tailwind framework.
-            <br />
-            Nice to meet you!
-          </div>
-        </div>
-        <p className="flex justify-center mt-4">Count: {state.count}</p>
-        <div className="flex justify-center mt-4 gap-x-4">
-          <button
-            className="bg-indigo-500 rounded-xl w-24 h-8 text-white"
-            onClick={() => dispatch({ type: "Add" })}
-          >
-            Add
-          </button>
-          <button
-            className="bg-indigo-500 rounded-xl w-24 h-8 text-white"
-            onClick={() => dispatch({ type: "Sub" })}
-          >
-            Subtract
-          </button>
+        <div className="md:col-start-2 mx-8 md:mx-20 text-justify">
+          <p className="text-center md:text-start text-xl font-semibold mt-8 md:mt-0 pb-3">
+            About me
+          </p>
+          My name is Supanut Wongtanom. I am studying at the faculty
+          of computer engineering in the 3rd year of international
+          program at KMUTT at the moment. I am interested in front-end 
+          development especially React JS, NextJS and CSS tailwind framework.
+          Nice to meet you!
         </div>
       </div>
+      <p className="flex justify-center mt-4">Count: {state.count}</p>
+      <div className="flex justify-center mt-4 gap-x-4">
+        <button
+          className="bg-indigo-500 rounded-xl w-24 h-8 text-white"
+          onClick={() => dispatch({ type: "Add" })}
+        >
+          Add
+        </button>
+        <button
+          className="bg-indigo-500 rounded-xl w-24 h-8 text-white"
+          onClick={() => dispatch({ type: "Sub" })}
+        >
+          Subtract
+        </button>
+      </div>
+    </div>
   );
 };
 
